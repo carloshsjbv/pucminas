e8fb1363931e952f6047b305476ec0fc848f12e3
André Machado <andregodan@gmail.com>
2022-06-19 01:46:27 -0300

    corrigindo problema de atualização da pagina

--- front-end/me-fit-web/next.config.js
+++ front-end/me-fit-web/next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
+}
