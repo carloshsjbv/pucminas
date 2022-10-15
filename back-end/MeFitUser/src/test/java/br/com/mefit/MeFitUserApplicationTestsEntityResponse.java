8e4840b788640deb0ee70cbcae452caa47180b31
Carlos Francisco <carloshsjbv@gmail.com>
2022-10-15 17:06:39 -0300

    Removendo teste

--- back-end/MeFitUser/src/test/java/br/com/mefit/MeFitUserApplicationTestsEntityResponse.java
+++ back-end/MeFitUser/src/test/java/br/com/mefit/MeFitUserApplicationTestsEntityResponse.java
@@ -1,12 +1,7 @@
 package br.com.mefit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MeFitUserApplicationTestsEntityResponse {
 
-	@Test
 	void contextLoads() {
 	}
 
