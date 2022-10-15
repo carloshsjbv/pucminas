3aa4f09e77eb09c6518f65d0d12c7ad9f7b0014b
Carlos Francisco <carloshsjbv@gmail.com>
2022-10-15 16:21:01 -0300

    Atualizando projeto plano-treinamento

--- back-end/plano-treinamento/plano-treinamento/src/main/java/com/puc/gerenciador/consultorias/plano/treinamento/repository/PlanoTreinoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puc.gerenciador.consultorias.plano.treinamento.repository;
-
-import com.puc.gerenciador.consultorias.plano.treinamento.entity.PlanoTreinoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanoTreinoRepository extends JpaRepository<PlanoTreinoEntity, Long> {
-}
