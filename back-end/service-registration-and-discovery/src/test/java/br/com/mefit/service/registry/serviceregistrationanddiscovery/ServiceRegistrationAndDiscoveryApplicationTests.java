94f25db47e4e496a64f589da00ee73dc5bc26401
Carlos Francisco <carloshsjbv@gmail.com>
2022-10-15 16:24:07 -0300

    Atualizando servico de service descovery

---
+++ back-end/service-registration-and-discovery/src/test/java/br/com/mefit/service/registry/serviceregistrationanddiscovery/ServiceRegistrationAndDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mefit.service.registry.serviceregistrationanddiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationAndDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
