3088ce2d423d524a540077ebf5e6f48ecf5be890
Carlos Francisco <carloshsjbv@gmail.com>
2022-11-18 00:43:36 -0300

    triggering pipeline by directory

--- back-end/service-registration-and-discovery/src/main/java/br/com/mefit/service/registry/serviceregistrationanddiscovery/ServiceRegistrationAndDiscoveryApplication.java
+++ back-end/service-registration-and-discovery/src/main/java/br/com/mefit/service/registry/serviceregistrationanddiscovery/ServiceRegistrationAndDiscoveryApplication.java
@@ -1,5 +1,6 @@
 package br.com.mefit.service.registry.serviceregistrationanddiscovery;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
